fda072af11c29036962e43067ff0d02413777eab
Oliver Säfström <safstrom.oliver@gmail.com>
2024-05-06 14:21:41 +0200

    Add getFinalRaceTime

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type RaceType string
@@ -25,6 +26,7 @@ type race struct {
 	RaceType  RaceType
 	StartTime string
 	EndTime   string
+	FinalTime time.Duration
 }
 
 func NewPerson(name string) *person {
@@ -34,11 +36,12 @@ func NewPerson(name string) *person {
 	}
 }
 
-func NewRace(RaceType RaceType, StartTime string, EndTime string) *race {
+func NewRace(RaceType RaceType, StartTime string, EndTime string, FinalTime time.Duration) *race {
 	return &race{
 		RaceType,
 		StartTime,
 		EndTime,
+		FinalTime,
 	}
 }
 
@@ -55,6 +58,7 @@ func main() {
 	for _, person := range participants {
 		fmt.Println(person)
 	}
+
 }
 
 func parsePerson(file *os.File) map[int]person {
@@ -89,16 +93,22 @@ func parsePerson(file *os.File) map[int]person {
 			continue
 		}
 
+		finalTime, err := getRaceTime(split[2], split[3])
+
+		if err != nil {
+			fmt.Printf("%v in line %v\n", err, lineNumber)
+			continue
+		}
+
 		person, ok := persons[id]
 
 		if ok {
-			newRace := NewRace(race, split[2], split[3])
+			newRace := NewRace(race, split[2], split[3], finalTime)
 			person.Races = append(person.Races, *newRace)
 			persons[id] = person
 		} else {
-
 			newPerson := NewPerson(split[0])
-			newRace := NewRace(race, split[2], split[3])
+			newRace := NewRace(race, split[2], split[3], finalTime)
 			newPerson.Races = append(newPerson.Races, *newRace)
 			persons[id] = *newPerson
 		}
@@ -107,6 +117,27 @@ func parsePerson(file *os.File) map[int]person {
 
 	file.Close()
 	return persons
+}
+
+func getRaceTime(start string, end string) (time.Duration, error) {
+	startTime, err := time.Parse("15:04:05", start)
+	if err != nil {
+		return 0, fmt.Errorf("Error parsing startTime: %v", err)
+	}
+
+	endTime, err := time.Parse("15:04:05", end)
+	if err != nil {
+		return 0, fmt.Errorf("Error parsing endTime: %v", err)
+	}
+
+	finalTime := endTime.Sub(startTime)
+
+	if finalTime <= 0 {
+		return 0, fmt.Errorf("Error parsing finalTime is less than 0: '%v'", err)
+	}
+
+	return finalTime, nil
+
 }
 
 func getRace(raceType string) (RaceType, error) {
