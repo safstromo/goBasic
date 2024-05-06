2b8991f1e1fb3d6383d4a2bca8dd015a11ed854f
Oliver Säfström <safstrom.oliver@gmail.com>
2024-05-06 10:42:23 +0200

    Add tests for getRace

--- main_test.go
+++ main_test.go
@@ -2,13 +2,37 @@ package main
 
 import "testing"
 
-func TestGetRace(t *testing.T) {
-	var eggRace = "eggRace"
+func TestGetRaceSuccess(t *testing.T) {
+	eggRace := "eggRace"
+	thousandMeters := "1000m"
+	sackRace := "sackRace"
 
-	result, err := getRace(eggRace)
+	eggResult, err := getRace(eggRace)
 
-	if result != EggRace || err != nil {
-		t.Errorf("Invalid result, got '%s', want '%s'", result, eggRace)
+	if eggResult != EggRace || err != nil {
+		t.Errorf("Invalid result, got '%s', want '%s'", eggResult, eggRace)
+	}
+
+	thousandResult, err := getRace(thousandMeters)
+
+	if thousandResult != Race1000m || err != nil {
+		t.Errorf("Invalid result, got '%s', want '%s'", thousandResult, eggRace)
+	}
+
+	sackResult, err := getRace(sackRace)
+
+	if sackResult != SackRace || err != nil {
+		t.Errorf("Invalid result, got '%s', want '%s'", sackResult, eggRace)
+	}
+
+}
+
+func TestGetRaceFail(t *testing.T) {
+
+	result, err := getRace("1234")
+
+	if result != "" && err == nil {
+		t.Errorf("Invalid result, got '%s' '%s' , want 'Unable to parse Race type", result, err)
 	}
 
 }
