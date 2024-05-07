31166d6a3db1d957323b95d12fecda6927b630cf
Oliver <safstrom.oliver@gmail.com>
2024-05-07 08:02:52 +0200

    Refactor structs

--- main.go
+++ main.go
@@ -10,59 +10,6 @@ import (
 	"time"
 )
 
-type RaceType string
-
-const (
-	Race1000m RaceType = "1000m"
-	EggRace   RaceType = "eggRace"
-	SackRace  RaceType = "sackRace"
-)
-
-type person struct {
-	Id    int
-	Name  string
-	Races []race
-}
-
-func (p person) getTotalTime() (time.Duration, error) {
-
-	if len(p.Races) != 3 {
-		return 0, fmt.Errorf("%v did not participate in all 3 races.", p.Name)
-	}
-
-	sum := 0 * time.Second
-
-	for _, race := range p.Races {
-		sum += race.FinalTime
-	}
-
-	return sum, nil
-}
-
-type race struct {
-	RaceType  RaceType
-	StartTime string
-	EndTime   string
-	FinalTime time.Duration
-}
-
-func NewPerson(name string, id int) *person {
-	return &person{
-		Id:    id,
-		Name:  name,
-		Races: make([]race, 0),
-	}
-}
-
-func NewRace(RaceType RaceType, StartTime string, EndTime string, FinalTime time.Duration) *race {
-	return &race{
-		RaceType,
-		StartTime,
-		EndTime,
-		FinalTime,
-	}
-}
-
 func main() {
 	file, err := os.Open("race-results.txt")
 
@@ -186,7 +133,6 @@ func getRaceTime(start string, end string) (time.Duration, error) {
 	}
 
 	return finalTime, nil
-
 }
 
 func getRace(raceType string) (RaceType, error) {
